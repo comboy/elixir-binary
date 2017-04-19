48be72540b5755d5ae3537b7f08f636704efc95c
comboy <kacper.ciesla@gmail.com>
2017-04-19 14:10:37 +0200

    mix skeleton

---
+++ lib/binary.ex
@@ -0,0 +1,18 @@
+defmodule Binary do
+  @moduledoc """
+  Documentation for Binary.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Binary.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/binary_test.exs
@@ -0,0 +1,8 @@
+defmodule BinaryTest do
+  use ExUnit.Case
+  doctest Binary
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
